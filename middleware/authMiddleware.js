c98b75f884de86b494e19f2d7fb38bb0e9b233e8
Cassia Ferraz <153029234+cassiaferraz@users.noreply.github.com>
2024-08-14 14:23:51 -0300

    middleware/server

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+ 
+function authenticateToken(req, res, next) {
+    const token = req.headers['x-access-token'];
+    if (!token) {
+        return res.status(401).json({ auth: false, message: 'Token não fornecido' });
+    }
+ 
+    jwt.verify(token, 'secreto', (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ auth: false, message: 'Falha na autenticação do token' });
+        }
+        req.userId = decoded.userId;
+        next();
+    });
+}
+ 
+module.exports =  authenticateToken
